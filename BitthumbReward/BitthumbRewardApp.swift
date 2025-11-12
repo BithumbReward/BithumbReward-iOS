d13dbabf7f8915e1c1514a5f974323d86cc4dc69
kangho <ibcylon@naver.com>
2025-11-12 13:43:54 +0900

    Initial Commit

---
+++ BitthumbReward/BitthumbRewardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitthumbRewardApp.swift
+//  BitthumbReward
+//
+//  Created by kangho on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BitthumbRewardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
