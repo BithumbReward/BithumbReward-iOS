3cd1389b5adbe144cc9b0b48a873b423b4b93d96
kangho <ibcylon@gmail.com>
2025-11-16 14:07:17 +0900

    refactor: myassetViewModel mvvm으로 변경
    # Conflicts:
    #	BitthumbReward/App/BitthumbRewardApp.swift

---
+++ BitthumbReward/Views/MyAssetsView/ViewModel/AssetRowViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  AssetRowViewModel.swift
+//  BitthumbReward
+//
+//  Created by kangho on 11/16/25.
+//
+
+import Foundation
+
+struct AssetRowViewModel: Identifiable {
+    private let account: Account
+    
+    var id: String {
+        account.currency
+    }
+    
+    init(account: Account) {
+        self.account = account
+    }
+    
+    let currentPrice: Double = 1000
+    
+    var currency: String = "BTC"
+    
+    var balance: Double = 0
+    
+    /// 원화 환산 금액 - balance - current price
+    var balanceKRW: Double {
+//        currentPrice * (Double(account.balance) ?? 0)
+        57698
+    }
+    
+    /// 등락폭 - current ticker / ave buy Price
+    var changeRate: Double {
+        return 0
+    }
+    
+    /// 등락금액 - 등락폭 * balance
+    var changePrice: Double {
+        (Double(account.balance) ?? 0) * changeRate
+    }
+    
+    var changeValue: String {
+        "+11,436(24.7%)"
+    }
+}
--- BitthumbReward/Views/MyAssetsView/ViewModel/MyAssetsViewModel.swift
+++ BitthumbReward/Views/MyAssetsView/ViewModel/MyAssetsViewModel.swift
@@ -9,4 +9,26 @@ import SwiftUI
 
 @Observable
 final class MyAssetsViewModel {
+    private let client: BithumbClient
+    var assetRows: [AssetRowViewModel] = []
+    
+    init(client: BithumbClient = BithumbClient(session: .shared)) {
+        self.client = client
+    }
+    
+    var balance: Double = 0
+    
+    func fetch() async {
+        do {
+            let accounts = try await client.account()
+            balance = Double(accounts.first(where: { $0.currency == "KRW" })?.balance ?? "0") ?? 0
+            assetRows = accounts
+                .filter { $0.currency != "KRW" }
+                .map { account in
+                    AssetRowViewModel(account: account)
+                }
+        } catch {
+            print(error)
+        }
+    }
 }
