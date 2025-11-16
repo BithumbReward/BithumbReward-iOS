7eef5b06bff1d009197b4f26d9fd34d8f90040aa
kangho <ibcylon@gmail.com>
2025-11-16 14:07:27 +0900

    add: historyView

--- BitthumbReward/Views/HistoryView/HistoryView.swift
+++ BitthumbReward/Views/HistoryView/HistoryView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct HistoryView: View {
+    @Environment(CoinListViewModel.self) var clViewModel
+    
     var body: some View {
-        Text("HistoryView")
+        Group {
+            if !clViewModel.listOfCoins.isEmpty {
+                listOfCoinsView
+            } else {
+                contentUnavailableView
+            }
+        }
+        .navigationTitle("코인 목록")
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.bithumbBackground)
+    }
+    
+    private var listOfCoinsView: some View {
+        List(clViewModel.listOfCoins) { coin in
+            NavigationLink(value: coin) { [vm = coin] in
+                CoinRowView(viewModel: vm)
+            }
+        }
+        .navigationDestination(for: CoinRowViewModel.self) { coin in
+            EmptyView()
+        }
+        .scrollContentBackground(.hidden)
+    }
+    
+    private var contentUnavailableView: some View {
+        ContentUnavailableView(
+            "코인 목록을 불러올 수 없어요.",
+            systemImage: "network.slash",
+            description: Text("잠시 후 다시 시도해주세요.")
+        )
     }
 }
 
